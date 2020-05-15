2e79ab77fc44eff4381191677d94e0b30ce88f80
Robert Yano <robertyano@gmail.com>
2020-05-14 22:07:20 -0300

    Added Choose File logic for Importing Accounts

--- app/src/main/java/com/ray/android/passwordsecurekiwi/ImportAccountActivity.java
+++ app/src/main/java/com/ray/android/passwordsecurekiwi/ImportAccountActivity.java
@@ -1,10 +1,16 @@
 package com.ray.android.passwordsecurekiwi;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
+
+import android.provider.OpenableColumns;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -26,33 +32,67 @@ import com.ray.android.passwordsecurekiwi.data.AccountLogin;
 
 public class ImportAccountActivity extends AppCompatActivity {
 
-    /**
-     * Database helper object  // ADDED THIS FOR TESTS
-     */
-    private AccountDbHelper mDbHelper;
+    TextView textFile;
 
+    private static final int PICKFILE_RESULT_CODE = 1;
+
+    // Called when the activity is first created
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.account_import);
 
         // Change the app bar to say "Import Accounts"
         setTitle(getString(R.string.import_accounts_overview));
 
-        // Setup CSV Download Button
-        AppCompatButton btn_import_csv = findViewById(R.id.btn_import_csv);
-        btn_import_csv.setOnClickListener(new View.OnClickListener() {
+
+        Button btn_choose_file = findViewById(R.id.btn_attach_file);
+        textFile = findViewById(R.id.file_attached);
+
+        btn_choose_file.setOnClickListener(new Button.OnClickListener(){
             @Override
-            public void onClick(View view) {
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("*/*");
+                startActivityForResult(intent,PICKFILE_RESULT_CODE);
+
+            }});
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case PICKFILE_RESULT_CODE:
+                if (resultCode == RESULT_OK) {
+                    String FilePath = data.getData().getPath();
+
+                    Uri returnUri = data.getData();
+                    Cursor returnCursor =
+                            getContentResolver().query(returnUri, null, null, null, null);
+
+                    /*
+                     * Get the column indexes of the data in the Cursor,
+                     * move to the first row in the Cursor, get the data,
+                     * and display it.
+                     */
+                    int nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+
+                    returnCursor.moveToFirst();
 
 
+                    Log.e("ImportAccountActivity","data: " + data );
 
+                    textFile.setText(returnCursor.getString(nameIndex) + " has been selected!");
 
+                    //textFile.setText(FilePath);
+                }
+                break;
 
-            }
-        });
-
-
-
+        }
     }
 }
+
+
+
